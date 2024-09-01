7495de598e5550cab24894ae2186cf3923fd0602
lougar <801387@qq.com>
2024-09-01 09:53:01 +0800

    test upload code change

--- cpp/cpptest/cpptest.cpp
+++ cpp/cpptest/cpptest.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 int main()
 {
-	//int a=0,b=0,c=0;
-	int t=0,n=0;
-	//scanf("%d,%d,%d", &a,&b,&c);
-	//printf("%d,%d,%d",a,b,c);
-	cin>>t>>n;
-	cout<<t*n<<endl;
+	int a=0,b=0,c=0;
+	//int t=0,n=0;
+	scanf("%d,%d,%d", &a,&b,&c);
+	printf("%d,%d,%d",a,b,c);
+	//cin>>t>>n;
+	//cout<<t*n<<endl;
 	return 0;
 } 
