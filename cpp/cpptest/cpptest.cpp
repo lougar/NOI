05e92d50c2091d626d0657c1beda2cfbc379a61b
lougar <801387@qq.com>
2024-09-01 08:14:00 +0800

    first upload

---
+++ cpp/cpptest/cpptest.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	//int a=0,b=0,c=0;
+	int t=0,n=0;
+	//scanf("%d,%d,%d", &a,&b,&c);
+	//printf("%d,%d,%d",a,b,c);
+	cin>>t>>n;
+	cout<<t*n<<endl;
+	return 0;
+} 
